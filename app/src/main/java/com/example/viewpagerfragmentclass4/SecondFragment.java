e900231047bf40556a40809b9e7a270a084f3ceb
Saveen <saveen047@gmail.com>
2021-09-14 14:40:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/viewpagerfragmentclass4/SecondFragment.java
@@ -0,0 +1,61 @@
+package com.example.viewpagerfragmentclass4;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class SecondFragment extends Fragment {
+
+    private TextView mtvData;
+    private MainActivity activity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        activity= (MainActivity) context;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mtvData.setText(activity.getData());
+    }
+
+    private void initViews(View view) {
+        mtvData=view.findViewById(R.id.tvData);
+
+
+
+    }
+}
